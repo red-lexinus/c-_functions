7456c505398fd037cbf7b47bf0790910c3c3f339
lexinus <starshenkov3@gmail.com>
2021-03-29 19:40:19 +0300

    rename_fun

---
+++ string_functions.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_CPP_STRING_FUNCTIONS_H
+#define MAIN_CPP_STRING_FUNCTIONS_H
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool check_in(string txt_1, string txt_2);
+void split(vector<string> &arr, string line, char space = ' ');
+
+#endif //MAIN_CPP_STRING_FUNCTIONS_H
