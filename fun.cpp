352b70ad4d7418007724ebd27be4d05433f8c350
lexinus <starshenkov3@gmail.com>
2021-03-29 19:30:22 +0300

    +2_fun
    bool check_in (проверка нахождения подстроки)
    void split (простой сплит, разделитель только char)

---
+++ fun.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool check_in(string txt_1, string txt_2) {
+    string txt = "";
+    int pos = 0;
+    for (auto chr: txt_1) {
+        if (txt == txt_2) return true;
+        else if (chr == txt_2[pos]) {
+            pos++;
+            txt = txt + chr;
+        } else {
+            pos = 0;
+            txt = "";
+        }
+    }
+    if (txt == txt_2) return true;
+    return false;
+}
+
+void split(vector<string> &arr, string line, char space = ' ') {
+    arr = {};
+    string txt = "";
+    for (auto chr: line) {
+        if (chr != space) txt = txt + chr;
+        else if (txt != "" and chr == space) {
+            arr.push_back(txt);
+            txt = "";
+        }
+    }
+    if (txt != "") arr.push_back(txt);
+}
+
---
+++ fun.hpp
@@ -0,0 +1,15 @@
+#ifndef MAIN_CPP_FUN_HPP
+#define MAIN_CPP_FUN_HPP
+
+#include <vector>
+#include <string>
+#include <typeinfo>
+
+using namespace std;
+
+bool check_in(string txt_1, string txt_2);
+
+void split(vector<string> &arr, string line, char space = ' ');
+
+
+#endif //MAIN_CPP_FUN_HPP
